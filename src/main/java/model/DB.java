a33cca8d7d8721eff987949186d695f183d81a13
hackamro <lounis.amar.unity@gmail.com>
2022-05-11 01:35:55 +0100

    create DB.java EntityManager using singleton pattren Signed-off-by: hackamro <lounis.amar.unity@gmail.com>

---
+++ src/main/java/model/DB.java
@@ -0,0 +1,34 @@
+package model;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class DB {
+	// --------------------------------------------- Entity Manager
+	private EntityManagerFactory emf;
+	public EntityManager em;
+
+	// --------------------------------------------- Dependency Inversion pattern
+	private DB() {
+		// MySql or h2 -> get configuration from persistence.xml
+		emf = Persistence.createEntityManagerFactory("mysql");
+		em = emf.createEntityManager();
+	}
+
+	// --------------------------------------------- singleton pattern
+	private static final Object LOCK = new Object();
+	private static DB db;
+
+	public static DB getInstanceDB() {
+		synchronized (LOCK) {
+			if (db == null) {
+				db = new DB();
+				System.out.println("********************* : singleton pattern");
+			}
+
+		}
+		return db;
+	}
+
+}
